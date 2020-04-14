c6a431097eb7bbee36853b11ecf2c1eef028eb49
unknown <gcobs135102@gmail.com>
2020-04-15 04:09:23 +0800

    feat: add validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
-import { Module } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule]
+  imports: [TypeOrmModule.forRoot(), UsersModule],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe
+    }
+  ]
 })
 export class AppModule {}
--- src/users/dtos/update-user.dto.ts
+++ src/users/dtos/update-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsString, IsOptional } from 'class-validator';
+
 export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
   firstName?: string;
+
+  @IsOptional()
+  @IsString()
   lastName?: string;
 }
