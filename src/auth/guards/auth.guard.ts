eb3780ba7f7202b9f228a0c1ec94736a4b550060
unknown <gcobs135102@gmail.com>
2020-08-16 15:36:20 +0800

    refactor(auth): remove necessary type alias

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,8 +1,6 @@
 import { Reflector } from '@nestjs/core';
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException, ForbiddenException } from '@nestjs/common';
-import { Request } from 'express';
 import { RequestWityUser } from '../typings/request-with-user';
-import { AuthPayload } from '../typings/auth.typings';
 import { AuthService } from '../services/auth.service';
 
 export const AUTH_GURARD_OPTIONS_METADAT_KEY = Symbol('Auth Guard Options');
@@ -16,17 +14,16 @@ export class AuthGuard implements CanActivate {
   constructor(private readonly reflector: Reflector, private readonly authService: AuthService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest<Request>();
+    const request = context.switchToHttp().getRequest<RequestWityUser>();
     const accessToken = request.headers.authorization?.replace(/^Bearer /, '');
-    let user: AuthPayload | undefined;
 
     if (!accessToken) {
       throw new UnauthorizedException();
     }
 
     try {
-      user = this.authService.verifyAccessToken(accessToken);
-      (request as RequestWityUser).user = user;
+      const user = this.authService.verifyAccessToken(accessToken);
+      request.user = user;
     } catch {
       throw new UnauthorizedException();
     }
@@ -39,7 +36,7 @@ export class AuthGuard implements CanActivate {
     if (options?.shouldBeSelfUserIdParam) {
       const userIdFromParam = request.params[options.shouldBeSelfUserIdParam];
 
-      if (userIdFromParam !== user.id) {
+      if (userIdFromParam !== request.user.id) {
         throw new ForbiddenException();
       }
     }
