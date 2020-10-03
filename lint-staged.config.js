605586913a0ab217e92abea9615f6b9ae53d620f
Jay Chen <gcobs135102@gmail.com>
2020-10-03 19:38:58 +0800

    chore: use lint-staged w/ nx correctly

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,13 @@
+const path = require('path');
+
 module.exports = {
-  '*.{js,ts,html,css,scss,json,md}': ['npm run format --uncommitted']
+  '*.{js,ts,html,css,scss,json,md}': files => {
+    const cwd = process.cwd();
+    const relativePaths = files.map(file => path.relative(cwd, file));
+
+    return [
+      `nx format:write --files="${relativePaths.join(',')}"`,
+      `nx affected:lint --files="${relativePaths.join(',')}" --fix --parallel`
+    ];
+  }
 };
