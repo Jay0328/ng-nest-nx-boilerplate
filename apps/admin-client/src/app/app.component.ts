90862b3d30d88c0a87cb00145303e6f2657ed4ea
Jay Chen <gcobs135102@gmail.com>
2020-10-03 18:14:53 +0800

    feat: generate libs/ui, apps/admin-client

---
+++ apps/admin-client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nnb-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'admin-client';
+}
---
+++ libs/ui/src/ui.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonDirective } from './button/button.directive';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ButtonDirective],
+  exports: [ButtonDirective]
+})
+export class UiModule {}
